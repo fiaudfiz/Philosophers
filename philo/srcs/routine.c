be54db5b5d89327c83c9e51ed9fbb7b2ad28323c
Michel <michelouali0@gmail.com>
2026-04-09 11:10:52 +0200

    fonction routine presque finie

--- philo/srcs/routine.c
+++ philo/srcs/routine.c
@@ -6,27 +6,48 @@ void *routine_thread(void *arg)
 
     if (tab->number % 2 != 0) //impair
     {
-        //mutex gauche
-        pthread_mutex_lock(tab->fork_left);
-        //mutex droit
-        pthread_mutex_lock(tab->fork_right);
-        //manger
-        print("manger");
-        sleep(tab->ptr->time_to_eat);
-        //update last meal
-        tab->time_since_last_meal = gettimeofday()
-        //dormir
-        print("dodo");
-        sleep(tab->ptr->time_to_sleep);
-        //dormir last_meal + time_to_die - time_to_eat
-        sleep(tab->time_since_last_meal + tab->ptr->time_to_die - tab->ptr->time_to_eat);
+        while (1)
+        {
+            //mutex gauche
+            pthread_mutex_lock(tab->fork_left);
+            //mutex droit
+            pthread_mutex_lock(tab->fork_right);
+            //manger
+            //penser aux mutex pour les print
+            print("manger");
+            pthread_mutex_unlock(tab->fork_left);
+            pthread_mutex_unlock(tab->fork_right);
+            sleep(tab->ptr->time_to_eat);
+            //update last meal
+            tab->time_since_last_meal = get_time_ms(); //fonction pour ramener un temps en micro secondes
+            //dormir
+            print("dodo");
+            sleep(tab->ptr->time_to_sleep);
+            //dormir last_meal + time_to_die - time_to_eat think revenir a la boucle sans forcement dormir le mutex lock va faire attendre
+            print("think");
+            sleep(tab->time_since_last_meal + tab->ptr->time_to_die - tab->ptr->time_to_eat); //peut etre pas oblige
+        }
     }
     else //pair
     {
-        //think(dormir) last_meal + time_to_die - time_to_eat
-        //gauche
-        //droite
-        //manger
-        //dormir
+        while (1)
+        {
+            //think(dormir) last_meal + time_to_die - time_to_eat
+            print("think");
+            //gauche
+            pthread_mutex_lock(tab->fork_left);
+            //droite
+            pthread_mutex_lock(tab->fork_right);
+            //manger
+            print("manger");
+            pthread_mutex_unlock(tab->fork_left);
+            pthread_mutex_unlock(tab->fork_right);
+            sleep(tab->ptr->time_to_eat);
+            //update last meal
+            tab->time_since_last_meal = get_time_ms();
+            //dormir
+            print("dodo");
+            sleep(tab->ptr->time_to_sleep);
+        }
     }
 }
