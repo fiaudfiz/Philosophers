5b3fd47dceba867cfb76176e53a7219a7f89bdf8
Michel <michelouali0@gmail.com>
2026-04-09 13:55:26 +0200

    ajout d'appels a la fonction exit

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: miouali <miouali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 11:05:04 by miouali           #+#    #+#             */
-/*   Updated: 2026/04/09 11:09:58 by miouali          ###   ########.fr       */
+/*   Updated: 2026/04/09 13:30:21 by miouali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,15 @@ int main(int ac, char **av)
     }
     global = malloc(sizeof(t_global_struct)); //struct de base
     if (!global)
-        return (0);
+    {
+        ERROR_AND_QUIT;
+    }
     if (parse_args(ac, av, global) != 0) //parser nombres/positifs //peut etre deja les ranger
         return (write(2, "Error\n", 6));
     global->tab = malloc (sizeof(t_tab_of_thread) * global->number_of_philo); //structure du tableau des threads
     if (!global->tab)
     {
+        ERROR_AND_QUIT;
         free (global);
         return (0);
     }
@@ -48,6 +51,7 @@ int main(int ac, char **av)
     global->fork = malloc(sizeof(pthread_mutex_t) * global->number_of_philo); //tableau des mutex
     if (!global->fork)
     {
+        ERROR_AND_QUIT;
         free(global->tab);
         free(global);
         return (0);
@@ -57,7 +61,7 @@ int main(int ac, char **av)
     //pk pas aussi encoyer les threads ET les mutex comme ca on init tout dedans //et aussi assigner les pointeurs de fork.
     pthread_create(tid, NULL, routine_body_guard(), global); //thread pour surveiller
     //on doit attendre que le thread body_guard envoie un signal
-    while (routine_body_guard() != 1)
+    while (body_guard(&global) != 1)
     {
     }
     free_and_exit(&global);
