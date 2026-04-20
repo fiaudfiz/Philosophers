ff6d2ff1940f7946f2dbe0737b50269f6635b2c0
Michel <michelouali0@gmail.com>
2026-04-20 13:25:36 +0200

    suppression du double de la fonction body_guard et ajout des mutex de print

--- philo/srcs/routine.c
+++ philo/srcs/routine.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miouali <miouali@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/20 13:23:01 by miouali           #+#    #+#             */
+/*   Updated: 2026/04/20 13:23:03 by miouali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
 void *routine_thread(void *arg)
@@ -11,22 +23,52 @@ void *routine_thread(void *arg)
         while (!tab->ptr->is_died) //ajouter le max de repas
         {
             //mutex gauche
+            if (tab->ptr->is_died != 0)
+                break;
             pthread_mutex_lock(tab->fork_left);
             //mutex droit
+            if (tab->ptr->is_died != 0)
+            {
+                //delock gauche
+                pthread_mutex_unlock(tab->fork_left);
+                break;
+            }
             pthread_mutex_lock(tab->fork_right);
             //manger
-            //penser aux mutex pour les print
+            if (tab->ptr->is_died != 0)
+            {
+                //delock les 2 mutex
+                pthread_mutex_unlock(tab->fork_left);
+                pthread_mutex_unlock(tab->fork_right);
+                break;
+            }
+            pthread_mutex_lock(tab->ptr->fork_print);
             print("manger");
+            pthread_mutex_unlock(tab->ptr->fork_print);
             pthread_mutex_unlock(tab->fork_left);
             pthread_mutex_unlock(tab->fork_right);
+            if (tab->ptr->is_died != 0)
+                break;
             sleep(tab->ptr->time_to_eat);
             //update last meal
             tab->time_since_last_meal = get_time_ms(); //fonction pour ramener un temps en micro secondes
             //dormir
+            if (tab->ptr->is_died != 0)
+                break;
+            pthread_mutex_lock(tab->ptr->fork_print);
             print("dodo");
+            pthread_mutex_unlock(tab->ptr->fork_print);
+            if (tab->ptr->is_died != 0)
+                break;
             sleep(tab->ptr->time_to_sleep);
             //dormir last_meal + time_to_die - time_to_eat think revenir a la boucle sans forcement dormir le mutex lock va faire attendre
+            if (tab->ptr->is_died != 0)
+                break;
+            pthread_mutex_lock(tab->ptr->fork_print);
             print("think");
+            pthread_mutex_unlock(tab->ptr->fork_print);
+            if (tab->ptr->is_died != 0)
+                break;
             sleep(tab->time_since_last_meal + tab->ptr->time_to_die - tab->ptr->time_to_eat); //peut etre pas oblige
         }
     }
@@ -35,48 +77,52 @@ void *routine_thread(void *arg)
         while (!tab->ptr->is_died) //ajouter le max de repas
         {
             //think(dormir) last_meal + time_to_die - time_to_eat
+            pthread_mutex_lock(tab->ptr->fork_print);
             print("think");
+            pthread_mutex_unlock(tab->ptr->fork_print);
             //gauche
+            if (tab->ptr->is_died != 0)
+                break;
             pthread_mutex_lock(tab->fork_left);
             //droite
+            if (tab->ptr->is_died != 0)
+            {
+                //delock gauche
+                pthread_mutex_unlock(tab->fork_left);
+                break;
+            }
             pthread_mutex_lock(tab->fork_right);
             //manger
+            if (tab->ptr->is_died != 0)
+            {
+                //delock les 2 mutex
+                pthread_mutex_unlock(tab->fork_left);
+                pthread_mutex_unlock(tab->fork_right);
+                break;
+            }
+            pthread_mutex_lock(tab->ptr->fork_print);
             print("manger");
+            pthread_mutex_unlock(tab->ptr->fork_print);
             pthread_mutex_unlock(tab->fork_left);
             pthread_mutex_unlock(tab->fork_right);
+            if (tab->ptr->is_died != 0)
+                break;
             sleep(tab->ptr->time_to_eat);
             //update last meal
             tab->time_since_last_meal = get_time_ms();
             //dormir
+            if (tab->ptr->is_died != 0)
+                break;
+            pthread_mutex_lock(tab->ptr->fork_print);
             print("dodo");
+            pthread_mutex_unlock(tab->ptr->fork_print);
+            if (tab->ptr->is_died != 0)
+                break;
             sleep(tab->ptr->time_to_sleep);
         }
     }
-    //qqun est mort donc agir ici
+    //normalement ici aucun mutex n'est lock donc on peut quit normalement
+    //quit
+    return ;
 }
 
-//a chaque print verifier que personne est mort
-
-void *routine_body_guard(void *arg)
-{
-    t_global_struct *global = (t_global_struct *)arg;
-    int i = 0;
-
-    while (i < global->number_of_philo && global->is_died == 0)
-    {
-        if (now() - global->tab[i].time_since_last_meal > global->time_to_die)
-        {
-            global->is_died = 1;
-            //mutex de print
-            print ("mort");
-            //mutex
-            break; //peut etre plus rapide car on sort direct mais peut etre besoin de sortir aussi du while (1)
-        }
-        i++;
-        //remettre i = 0;
-        sleep(0.001);
-        //sleep 1ms
-    }
-    //ici des que un meurt chaque thread va s'arreter tout seul
-    //on doit arreter ce thread
-}
