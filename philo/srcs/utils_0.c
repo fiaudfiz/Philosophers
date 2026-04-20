333b4ce1b691248de6d703be0772167f68dfa132
Michel <michelouali0@gmail.com>
2026-04-20 13:26:08 +0200

    ajout de la creation du mutex des print

--- philo/srcs/utils_0.c
+++ philo/srcs/utils_0.c
@@ -6,7 +6,7 @@
 /*   By: miouali <miouali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 11:04:49 by miouali           #+#    #+#             */
-/*   Updated: 2026/04/09 14:29:51 by miouali          ###   ########.fr       */
+/*   Updated: 2026/04/20 11:46:05 by miouali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,9 @@ void    init_variables(t_global_struct *global, t_tab_of_thread *tab)
             exit(global, tab);
         i++;
     }
+    //creation du mutex de print
+    if (pthread_mutex_init(&global->fork_print, NULL) != 0)
+        exit(global, tab);
     i = 0;
     while (i < global->number_of_philo)
     {
