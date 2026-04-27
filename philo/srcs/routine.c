c4cb48d1ca35279d650ebf47fd0c9be15a5c44de
Michel <michelouali0@gmail.com>
2026-04-27 19:59:11 +0200

    ajout du nombre max de repas

--- philo/srcs/routine.c
+++ philo/srcs/routine.c
@@ -6,7 +6,7 @@
 /*   By: miouali <miouali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 13:23:01 by miouali           #+#    #+#             */
-/*   Updated: 2026/04/27 10:44:58 by miouali          ###   ########.fr       */
+/*   Updated: 2026/04/27 19:24:46 by miouali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,86 @@ void *routine_thread(void *arg)
 
     tab->tid = pthread_self();
     //remplir le tid
-    if (tab->number % 2 != 0) //impair
+
+    //cas ou cest le dernier et on a un nombre impair
+    if ((tab->ptr->number_of_philo % 2 != 0) && (tab->number == tab->ptr->number_of_philo))
     {
-        while (!tab->ptr->is_died) //ajouter le max de repas
+        while (!tab->ptr->is_died || tab->number_of_eat != tab->ptr->max_eat) //ajouter le max de repas
         {
-            //mutex a ajouter
+            usleep(tab->ptr->time_to_eat * 1000);
+           pthread_mutex_lock(tab->ptr->fork_is_died);
             if (tab->ptr->is_died != 0)
+            {
+                pthread_mutex_unlock(tab->ptr->fork_is_died);
                 break;
-            // mutex
+            }
+            pthread_mutex_unlock(tab->ptr->fork_is_died);
+            pthread_mutex_lock(tab->fork_right);
+            print_philo(tab->ptr, tab->number, 1);
+            pthread_mutex_lock(tab->fork_left);
+            print_philo(tab->ptr, tab->number, 2);
+            pthread_mutex_lock(tab->ptr->fork_is_died);
+            if (tab->ptr->is_died != 0)
+            {
+                pthread_mutex_unlock(tab->fork_right);
+                pthread_mutex_unlock(tab->fork_left);
+                pthread_mutex_unlock(tab->ptr->fork_is_died);
+                break;
+            }
+            pthread_mutex_unlock(tab->ptr->fork_is_died);
+            print_philo(tab->ptr, tab->number, 3);
+            pthread_mutex_lock(tab->ptr->fork_last_meal);
+            tab->time_since_last_meal = get_time_ms(); //fonction pour ramener un temps en micro secondes
+            pthread_mutex_unlock(tab->ptr->fork_last_meal);
+            usleep(tab->ptr->time_to_eat * 1000);
+            pthread_mutex_unlock(tab->fork_right);
+            pthread_mutex_unlock(tab->fork_left);
+            pthread_mutex_lock(tab->ptr->mutex_meal);
+            tab->number_of_eat += 1;
+            pthread_mutex_unlock(tab->ptr->mutex_meal);
+            print_philo(tab->ptr, tab->number, 4);
+            pthread_mutex_lock(tab->ptr->fork_is_died);
+            if (tab->ptr->is_died != 0)
+            {
+                pthread_mutex_unlock(tab->ptr->fork_is_died);
+                break;
+            }
+            pthread_mutex_unlock(tab->ptr->fork_is_died);
+            usleep(tab->ptr->time_to_sleep * 1000);
+            print_philo(tab->ptr, tab->number, 5);
+            pthread_mutex_lock(tab->ptr->fork_is_died);
+            if (tab->ptr->is_died != 0)
+            {
+                pthread_mutex_unlock(tab->ptr->fork_is_died); 
+                break;
+            }
+            pthread_mutex_unlock(tab->ptr->fork_is_died);
+        }
+    }
+    else if (tab->number % 2 != 0) //impair
+    {
+        while (!tab->ptr->is_died || tab->number_of_eat != tab->ptr->max_eat) //ajouter le max de repas
+        {
+           pthread_mutex_lock(tab->ptr->fork_is_died);
+            if (tab->ptr->is_died != 0)
+            {
+                pthread_mutex_unlock(tab->ptr->fork_is_died);
+                break;
+            }
+            pthread_mutex_unlock(tab->ptr->fork_is_died);
             pthread_mutex_lock(tab->fork_left);
             print_philo(tab->ptr, tab->number, 2);
             pthread_mutex_lock(tab->fork_right);
-            print_philo(tab->ptr, tab->number, 2);
-            //mutex
+            print_philo(tab->ptr, tab->number, 1);
+            pthread_mutex_lock(tab->ptr->fork_is_died);
             if (tab->ptr->is_died != 0)
             {
                 pthread_mutex_unlock(tab->fork_left);
                 pthread_mutex_unlock(tab->fork_right);
+                pthread_mutex_unlock(tab->ptr->fork_is_died);
                 break;
             }
-            //mutex
+            pthread_mutex_unlock(tab->ptr->fork_is_died);
             print_philo(tab->ptr, tab->number, 3);
             pthread_mutex_lock(tab->ptr->fork_last_meal);
             tab->time_since_last_meal = get_time_ms(); //fonction pour ramener un temps en micro secondes
@@ -45,42 +105,69 @@ void *routine_thread(void *arg)
             usleep(tab->ptr->time_to_eat * 1000);
             pthread_mutex_unlock(tab->fork_left);
             pthread_mutex_unlock(tab->fork_right);
+            pthread_mutex_lock(tab->ptr->mutex_meal);
+            tab->number_of_eat += 1;
+            pthread_mutex_unlock(tab->ptr->mutex_meal);
             print_philo(tab->ptr, tab->number, 4);
 
-            //mutex
+            pthread_mutex_lock(tab->ptr->fork_is_died);
             if (tab->ptr->is_died != 0)
+            {
+                pthread_mutex_unlock(tab->ptr->fork_is_died);
                 break;
-            //mutex
+            }
+            pthread_mutex_unlock(tab->ptr->fork_is_died);
             usleep(tab->ptr->time_to_sleep * 1000);
             print_philo(tab->ptr, tab->number, 5);
-            //mutex
+            pthread_mutex_lock(tab->ptr->fork_is_died);
             if (tab->ptr->is_died != 0)
-                 break;
-            //mutex
+            {
+                pthread_mutex_unlock(tab->ptr->fork_is_died); 
+                break;
+            }
+            pthread_mutex_unlock(tab->ptr->fork_is_died);
+            usleep(tab->ptr->time_to_eat * 1000);
         }
     }
     else //pair
     {
-        while (!tab->ptr->is_died) //ajouter le max de repas
+        while (!tab->ptr->is_died || tab->number_of_eat != tab->ptr->max_eat) //ajouter le max de repas
         {
             print_philo(tab->ptr, tab->number, 5);
+            
+            pthread_mutex_lock(tab->ptr->fork_is_died);
             if (tab->ptr->is_died != 0)
+            {
+                pthread_mutex_unlock(tab->ptr->fork_is_died);
                 break;
+            }
+            pthread_mutex_unlock(tab->ptr->fork_is_died);
+            
             pthread_mutex_lock(tab->fork_right);
-            print_philo(tab->ptr, tab->number, 2);
+            print_philo(tab->ptr, tab->number, 1);
+            
+            pthread_mutex_lock(tab->ptr->fork_is_died);
             if (tab->ptr->is_died != 0)
             {
                 pthread_mutex_unlock(tab->fork_right);
+                pthread_mutex_unlock(tab->ptr->fork_is_died);
                 break;
             }
+            pthread_mutex_unlock(tab->ptr->fork_is_died);
+
             pthread_mutex_lock(tab->fork_left);
             print_philo(tab->ptr, tab->number, 2);
+            
+            pthread_mutex_lock(tab->ptr->fork_is_died);
             if (tab->ptr->is_died != 0)
             {
                 pthread_mutex_unlock(tab->fork_right);
                 pthread_mutex_unlock(tab->fork_left);
+                pthread_mutex_unlock(tab->ptr->fork_is_died);
                 break;
             }
+            pthread_mutex_unlock(tab->ptr->fork_is_died);
+            
             print_philo(tab->ptr, tab->number, 3);
             pthread_mutex_lock(tab->ptr->fork_last_meal);
             tab->time_since_last_meal = get_time_ms();
@@ -88,12 +175,29 @@ void *routine_thread(void *arg)
             usleep(tab->ptr->time_to_eat * 1000);
             pthread_mutex_unlock(tab->fork_left);
             pthread_mutex_unlock(tab->fork_right);
+            pthread_mutex_lock(tab->ptr->mutex_meal);
+            tab->number_of_eat += 1;
+            pthread_mutex_unlock(tab->ptr->mutex_meal);
+            pthread_mutex_lock(tab->ptr->fork_is_died);
             if (tab->ptr->is_died != 0)
+            {
+                pthread_mutex_unlock(tab->ptr->fork_is_died);    
                 break;
+            }
+            pthread_mutex_unlock(tab->ptr->fork_is_died);
+            
             print_philo(tab->ptr, tab->number, 4);
+            
+            pthread_mutex_lock(tab->ptr->fork_is_died);
             if (tab->ptr->is_died != 0)
+            {
+                pthread_mutex_unlock(tab->ptr->fork_is_died);    
                 break;
+            }
+            pthread_mutex_unlock(tab->ptr->fork_is_died);
+            
             usleep(tab->ptr->time_to_sleep * 1000);
+            usleep(tab->ptr->time_to_eat * 1000);
         }
     }
     return (NULL);
