4d59692e0198665dc0ae0ef34902d426bd2f2bdc
Michel <michelouali0@gmail.com>
2026-04-27 10:45:25 +0200

    ajouts de nouvelles fonctions dans le .h:

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: miouali <miouali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 11:05:13 by miouali           #+#    #+#             */
-/*   Updated: 2026/04/20 14:11:53 by miouali          ###   ########.fr       */
+/*   Updated: 2026/04/24 18:59:55 by miouali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,12 +36,14 @@ typedef struct s_global_struct
     long    time_to_eat;
     long    time_to_sleep;
     long    time_to_die;
+    long    start;
     int     max_eat;
     int     is_died;
     t_tab_of_thread    *tab;
     int     number_of_philo;
     pthread_mutex_t      *fork;
     pthread_mutex_t       *fork_print;
+    pthread_mutex_t         *fork_last_meal;
 }   t_global_struct ;
 
 void    prog_usage(void);
@@ -52,5 +54,6 @@ void *routine_thread(void *arg);
 void    exit_philo(t_global_struct *global);
 void *routine_body_guard(void *arg);
 int parse_args(int ac, char **av, t_global_struct *global);
+void    print_philo(t_global_struct *global, int number, int mode);
 
 #endif
