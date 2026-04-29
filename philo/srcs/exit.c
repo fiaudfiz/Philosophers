2358aa24a8de94c808ac641edd7923d13fa79985
Michel <michelouali0@gmail.com>
2026-04-29 15:00:31 +0200

    suppression des leaks

--- philo/srcs/exit.c
+++ philo/srcs/exit.c
@@ -6,15 +6,12 @@
 /*   By: miouali <miouali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 13:35:16 by miouali           #+#    #+#             */
-/*   Updated: 2026/04/28 11:15:59 by miouali          ###   ########.fr       */
+/*   Updated: 2026/04/29 14:13:43 by miouali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-//cette fonction est appelee apres que les threads sont arretes donc c juste du free
-//detruire les mutex (mutex de print aussi)
-//liberer ressources
 void    exit_philo(t_global_struct *global)
 {
     int i = 1;
@@ -38,6 +35,14 @@ void    exit_philo(t_global_struct *global)
         pthread_mutex_destroy(global->mutex_meal);
     if (global->tab)
         free(global->tab);
+    if (global->fork_print)
+        free (global->fork_print);
+    if (global->fork_last_meal)
+        free (global->fork_last_meal);
+    if (global->fork_is_died)
+        free (global->fork_is_died);
+    if (global->mutex_meal)
+        free (global->mutex_meal);
     if (global)
         free(global);
     return ;
